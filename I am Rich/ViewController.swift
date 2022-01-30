5d49ba215ea962f8ebde75d15d8c4a2cedc132e4
William Daniel da Silva Kuhs <wdarchenemy@gmail.com>
2022-01-30 14:24:42 -0300

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by William Daniel da Silva Kuhs on 30/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
